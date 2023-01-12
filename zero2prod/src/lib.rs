5cdfa02bab924dec03aba55b26c64829087e26b1
Mark Westerweel <mark.westerweel@hva.nl>
2023-01-12 12:06:54 +0100

    itegration test for health_check

---
+++ zero2prod/src/lib.rs
@@ -0,0 +1,21 @@
+use actix_web:: { web, App, HttpServer, Responder, HttpResponse };
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+
+async fn health_check()-> impl Responder{
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    //need a server start one.. with options first defined??
+    let server = HttpServer::new( || {
+        //start app
+        App::new()
+        .route("/health_check", web::get().to(health_check))        
+    })
+    .listen(listener)? //error handling shorthand for the Result<()> enum
+    .run();// run the server 
+    // .await // interesting placement
+    Ok(server)
+}
--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,23 +1,10 @@
-use actix_web:: { web, App, HttpRequest, HttpServer, Responder };
+use std::net::TcpListener;
 
-
-//  helper function
-async fn greet(req: HttpRequest) -> impl Responder{
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    //need a server start one.. with options first defined??
-    HttpServer::new( || {
-        //start app
-        App::new()
-        //chain routes and call helper
-        .route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))        
-    })
-    .bind("127.0.0.1:8000")? //error handling shorthand for the Result<()> enum
-    .run() // run the server 
-    .await // interesting placement
+async fn main() -> std::io::Result<()>{
+    let listener = TcpListener::bind("127.0.0.1.:0").expect("failed to bind address");
+    run(listener)?.await
 }
+
